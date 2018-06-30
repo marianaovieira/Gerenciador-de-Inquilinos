fc7166779a74b6fd01c8e10f064bb536622e7f3e
Asus <marianaovieira1@gmail.com>
2018-06-30 11:34:09 -0300

    Início do Projeto
    Signed-off-by: Asus <marianaovieira1@gmail.com>

---
+++ app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Login.java
@@ -0,0 +1,13 @@
+package br.iff.pooa20181.gerenciadordeinquilinos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
