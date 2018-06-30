5e49c630def3ea40980278f8caf03c54b33917da
Mariana Vieira <marianaovieira1@gmail.com>
2018-06-30 12:01:10 -0300

    Configuração página login
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

---
+++ app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Main.java
@@ -0,0 +1,27 @@
+package br.iff.pooa20181.gerenciadordeinquilinos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnInquilino = (Button) findViewById(R.id.btn_Cadastro_Inq);
+
+        btnInquilino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(getContext(), LivroActivity.class);
+                //startActivity(intent);
+
+            }
+        });
+    }
+}
