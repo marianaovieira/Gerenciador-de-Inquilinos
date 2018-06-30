cdcd48e466b08d82809290a50637f010229ed04e
Mariana Vieira <marianaovieira1@gmail.com>
2018-06-30 16:37:12 -0300

    Correção de erros
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Main.java
+++ app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Main.java
@@ -1,5 +1,6 @@
 package br.iff.pooa20181.gerenciadordeinquilinos;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,10 +19,16 @@ public class Main extends AppCompatActivity {
         btnInquilino.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Intent intent = new Intent(getContext(), LivroActivity.class);
-                //startActivity(intent);
+                Intent intent = new Intent(getContext(), Inquilinos.class);
+                startActivity(intent);
 
             }
         });
     }
+
+    private Context getContext(){
+        return this;
+    }
+
 }
+
