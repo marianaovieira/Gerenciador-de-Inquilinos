f9b307c62415a3b1bcfada42af17aaa35da133ad
Mariana Vieira <marianaovieira1@gmail.com>
2018-06-30 14:11:20 -0300

    RecyclerView
    Signed-off-by: Mariana Vieira <marianaovieira1@gmail.com>

--- app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Inquilinos.java
+++ app/src/main/java/br/iff/pooa20181/gerenciadordeinquilinos/Inquilinos.java
@@ -1,14 +1,30 @@
 package br.iff.pooa20181.gerenciadordeinquilinos;
 
 import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class Inquilinos extends AppCompatActivity {
+import io.realm.Realm;
+
+
+public class Inquilinos extends AppCompatActivity implements ClickRecyclerViewListener{
+
+    private Realm realm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inquilinos);
+
+
+
+
 
 
     }
